79d3598d4f52dada5cfe45c8132c78fd1f5ce03c
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-09-05 20:21:26 +0300

    Third

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ console.log('Second');
 let dateSecond = '2025-12-31';
 console.log(dateSecond.replace(/(2025)-(12)-(31)/g, '$3/$2/$1'));
 
+//Third
+console.log('Third');
+let strThird = 'Я учу javascript!';
+//substring
+console.log(strThird.substring(2, 5));
+console.log(strThird.substring(6, 16));
+//substr
+console.log(strThird.substr(2, 3));
+console.log(strThird.substr(6, 10));
+//slice
+console.log(strThird.substring(2, 5));
+console.log(strThird.substring(6, 16));
