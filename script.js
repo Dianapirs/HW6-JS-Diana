8260028c75a49bdcce7a59c6138d5223370d96cd
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-09-05 20:14:06 +0300

    Second

--- script.js
+++ script.js
@@ -2,3 +2,9 @@
 console.log('First');
 let strFirst = 'aaa@bbb@ccc';
 console.log(strFirst.replace(/@/g, '!'));
+
+//Second
+console.log('Second');
+let dateSecond = '2025-12-31';
+console.log(dateSecond.replace(/(2025)-(12)-(31)/g, '$3/$2/$1'));
+
